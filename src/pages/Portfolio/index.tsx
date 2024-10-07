714a8b1d0b020a74e3ca7e1cb4ee77501e5b2540
Arman Bolatuly <arman_bulatovich@mail.ru>
2024-10-07 23:07:57 +0500

    feat: added new pages for user

---
+++ src/pages/Portfolio/index.tsx
@@ -0,0 +1,13 @@
+import { Title } from '@mantine/core'
+
+const Portfolio = () => {
+  return (
+    <div>
+      <Title order={3} c="#FFFFFF">
+        Portfolio
+      </Title>
+    </div>
+  )
+}
+
+export default Portfolio
---
+++ src/pages/Wallets/index.tsx
@@ -0,0 +1,13 @@
+import { Title } from '@mantine/core'
+
+const Wallets = () => {
+  return (
+    <div>
+      <Title order={3} c="#FFFFFF">
+        Wallets
+      </Title>
+    </div>
+  )
+}
+
+export default Wallets
