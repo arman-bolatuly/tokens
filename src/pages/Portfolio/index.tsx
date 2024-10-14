edf9aef5f0789ce3f94f8e4539d61bcc1cd1702a
Arman Bolatuly <arman_bulatovich@mail.ru>
2024-10-14 21:21:16 +0500

    feat: refactoring

--- src/pages/Portfolio/index.tsx
+++ src/pages/Portfolio/index.tsx
@@ -1,51 +1,44 @@
-import { useState } from 'react'; 
-import { Avatar as MantineAvatar, SegmentedControl, Title } from '@mantine/core';
-import UserAvatar from './Avatar';
-import First from './First';
-import SecondPortfolio from './SecondPortfolio';
+import { SegmentedControl, Title, Text, Card, Button } from '@mantine/core'
+import { IconArrowRight } from '@tabler/icons-react'
+
+import First from './First'
 
 function Portfolio() {
-  const [opened, setOpened] = useState(false);
-  const avatarSrc = ""; 
-  const userName = "Erlan Ibatulliev";
-
-  const openModal = () => {
-    setOpened(true);
-  };
-
   return (
-    <div className="relative h-screen p-4">
-      <div className="flex items-center absolute top-4 right-4 cursor-pointer">
-        <MantineAvatar
-          variant="filled"
-          radius="xl"
-          src={avatarSrc}
-          onClick={openModal}
-          className="cursor-pointer"
-        />
-        <div onClick={openModal} className="ml-3 text-white text-lg font-medium">
-          {userName}
-        </div>
-      </div>
-
-      <Title order={3} className="text-white mt-10">
+    <div className="flex flex-col space-y-6">
+      <Title order={3} className="text-[#FFFFFF]">
         Portfolio
       </Title>
+      <First />
+      <div className="p-4 bg-white rounded-[10px]">
+        <Title order={4}>Portfolio Value</Title>
+        <Text size="xl" className="mt-2">
+          $0.00
+        </Text>
+      </div>
+      <Card shadow="sm" component="a" href="#" h={84}>
+        <div className="flex justify-between items-center">
+          <div>
+            <Title order={6}>
+              Hamilton Lane's Record-Breaking $5.6B Secondaries Fund - Now
+              Exclusively on Securitize
+            </Title>
 
-      <UserAvatar 
-        opened={opened}
-        onClose={() => setOpened(false)}
-        userName={userName}
-        avatarSrc={avatarSrc}
-      />
+            <Text mt={12}>
+              Access stability and quicker returns with a secondary fund that
+              acquires mature private equity investments, often at prices below
+              market value.
+            </Text>
+          </div>
 
-      <First /> 
-
-      <SecondPortfolio />
-
-      <SegmentedControl data={['My Assets','In Progres']} className='mt-4' />;
+          <Button variant="transparent" rightSection={<IconArrowRight />}>
+            View the Fund
+          </Button>
+        </div>
+      </Card>
+      <SegmentedControl data={['My Assets', 'In Progres']} />;
     </div>
-  );
+  )
 }
 
-export default Portfolio;
+export default Portfolio
